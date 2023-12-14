16084fdd8524aa016a4c5fc3901ff3eb7c10e03f
Julio Gomes <juliogomesj09@gmail.com>
2023-12-14 20:26:59 -0300

    criando a sessao de projetos

---
+++ src/components/Projeto/styles.ts
@@ -0,0 +1,17 @@
+import Styled from 'styled-components'
+
+export const Card = Styled.div`
+border: 1px solid #C1C1C1;
+padding: 16px;
+`
+
+export const LinkBotao = Styled.a`
+color: #fff;
+font-size: 14px;
+background-color: #4476bf;
+text-decoration: none;
+padding: 8px;
+display: inline-block;
+margin-top: 24px;
+
+`
--- src/styles.ts
+++ src/styles.ts
@@ -5,11 +5,12 @@ const EstiloGlobal = createGlobalStyle`
   margin: 0;
   padding: 0;
   font-family: 'Inter', sans-serif;
+  list-style: none;
 }
 
 body{
   padding-top: 80px;
-  
+
   @media (max-width: 768px){
     padding-top: 16px
   }
